8dc21ff93ecc4e6977cba4516a7f8e11563864c0
yvan vander sanden <yvan@attr-x.net>
2018-06-30 13:00:39 +0200

    Eerste versie van deze template

---
+++ oefening-template1/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace First
+{
+	class Program
+	{
+		/* instructies
+		 * 
+		 */
+		public static int NaamOefening(int a, int b)
+		{
+			// schrijf hier je code
+			return 0;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Resultaat van oefening 1: "  + NaamOefening(1, 3));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ oefening-template1/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using NUnitLite;
+
+namespace Tests
+{
+	[TestFixture]
+	public class TestClass
+	{
+		// deze functie is nodig om de testen achteraf via de webinterface uit te voeren
+		public static int Run(string resultPath)
+		{
+			string[] args = { "--work=" + resultPath };
+			return new AutoRun().Execute(args);
+		}
+
+		[Test]
+		public void TestOefening1()
+		{
+			Assert.That(First.Program.NaamOefening(1, 2), Is.EqualTo(3), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+			Assert.That(First.Program.NaamOefening(20, -3), Is.EqualTo(17), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+		}
+	}
+}
